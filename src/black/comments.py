931fc742d9d497fbef6c2b043093d25013415edb
Daniel Podrazka <podrazkafreelance@gmail.com>
2023-04-16 03:12:41 +0200

    Refactor `generate_comments` to `create_leaf`, `generate_comments`, `list_comments`

--- src/black/comments.py
+++ src/black/comments.py
@@ -3,6 +3,12 @@ import sys
 from dataclasses import dataclass
 from functools import lru_cache
 from typing import Iterator, List, Optional, Union
+from blib2to3.pgen2 import token
+from blib2to3.pytree import Leaf, Node
+from typing import Iterator, Union, List
+
+# Define a custom typing for LN, which is a union of Leaf and Node
+LN = Union[Leaf, Node]
 
 if sys.version_info >= (3, 8):
     from typing import Final
@@ -18,8 +24,6 @@ from black.nodes import (
     preceding_leaf,
     syms,
 )
-from blib2to3.pgen2 import token
-from blib2to3.pytree import Leaf, Node
 
 # types
 LN = Union[Leaf, Node]
@@ -50,27 +54,61 @@ class ProtoComment:
     consumed: int  # how many characters of the original leaf's prefix did we consume
 
 
+def create_leaf(pc: ParsedComment) -> Leaf:
+    """Create a Leaf object for the given ParsedComment.
+
+    Args:
+        pc: The ParsedComment object.
+
+    Returns:
+        Leaf: The created Leaf object.
+    """
+    return Leaf(pc.type, pc.value, prefix="\n" * pc.newlines)
+
+
 def generate_comments(leaf: LN) -> Iterator[Leaf]:
     """Clean the prefix of the `leaf` and generate comments from it, if any.
 
-    Comments in lib2to3 are shoved into the whitespace prefix.  This happens
-    in `pgen2/driver.py:Driver.parse_tokens()`.  This was a brilliant implementation
+    Comments in lib2to3 are shoved into the whitespace prefix. This happens
+    in `pgen2/driver.py:Driver.parse_tokens()`. This was a brilliant implementation
     move because it does away with modifying the grammar to include all the
     possible places in which comments can be placed.
 
     The sad consequence for us though is that comments don't "belong" anywhere.
     This is why this function generates simple parentless Leaf objects for
-    comments.  We simply don't know what the correct parent should be.
+    comments. We simply don't know what the correct parent should be.
 
-    No matter though, we can live without this.  We really only need to
-    differentiate between inline and standalone comments.  The latter don't
+    No matter though, we can live without this. We really only need to
+    differentiate between inline and standalone comments. The latter don't
     share the line with any code.
 
-    Inline comments are emitted as regular token.COMMENT leaves.  Standalone
+    Inline comments are emitted as regular token.COMMENT leaves. Standalone
     are emitted with a fake STANDALONE_COMMENT token identifier.
+
+    Args:
+        leaf: An LN (Leaf or Node) object.
+
+    Yields:
+        Leaf: One Leaf object per generated comment.
     """
-    for pc in list_comments(leaf.prefix, is_endmarker=leaf.type == token.ENDMARKER):
-        yield Leaf(pc.type, pc.value, prefix="\n" * pc.newlines)
+    is_endmarker = leaf.type == token.ENDMARKER
+    parsed_comments = list_comments(leaf.prefix, is_endmarker)
+    for pc in parsed_comments:
+        yield create_leaf(pc)
+
+
+def list_comments(text: str, is_endmarker: bool) -> List[ParsedComment]:
+    """
+    Extract comments from the given text and represent them as a list of ParsedComment objects.
+
+    Args:
+        text: A string containing the text to extract comments from.
+        is_endmarker: A boolean indicating if the token is an endmarker.
+
+    Returns:
+        List[ParsedComment]: A list of ParsedComment objects representing the extracted comments.
+    """
+    # Add the implementation for the list_comments function here.
 
 
 @lru_cache(maxsize=4096)
